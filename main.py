c1e17e4439069a5086db852275d2829da8efcef4
yatomikazuchi <caioevilabner@gmail.com>
2024-09-27 14:36:38 -0300

    base code created

--- main.py
+++ main.py
@@ -2,3 +2,22 @@ import cv2
 import ascii_magic
 import imgkit
 import os
+
+#create folders
+os.mkdir("images")
+os.mkdir("acii")
+os.mkdir("html")
+
+#get video
+video = ""
+video_obj = cv2.VideoCapture(video)
+
+count = 0
+flag = 1
+while flag:
+    flag, image = video_obj.read()
+    try:
+        cv2.imwrite("images/frame$d.jpg" % count, image)
+    except:
+        break
+    count += 1
